016845f43ce322eb20232c3f8202fb7912eb6431
Thieu Luu <96815689+tluu5@users.noreply.github.com>
2024-09-24 14:42:21 +0000

    commit to finish

--- app/controllers/addition_controller.rb
+++ app/controllers/addition_controller.rb
@@ -1,11 +1,11 @@
 class AdditionController < ApplicationController
   def show_addition_form
-    render({ :template => "addition_templates/add_form" })
+    render({ :template => "addition_templates/addition_form" })
   end
 
   def add_these
     @first_number = params.fetch("first_number").to_f
-    @second_number = params.fetch("second_num").to_f
+    @second_number = params.fetch("second_number").to_f
 
     @result = @first_number + @second_number
 
